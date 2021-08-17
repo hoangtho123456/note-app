fce6c035c671463c5e95a07838248da8cae04284
hoangtho123456 <danghoangtho1994@gmail.com>
2021-08-17 21:15:35 +0700

    hide button add when change route

--- src/containers/header-app.js
+++ src/containers/header-app.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Button from '../components/button';
-
+import { useLocation } from 'react-router-dom';
 const HeaderApp = ({title, onShowAddTask, showAddTask}) => {
-    // const onCLick = () => {
-    //     console.log('click');
-    // };
+    const location = useLocation();
     
     return (
         <section className="py-3 flex flex-wrap items-center justify-between">
             <h1 className="text-2xl md:text-3xl font-bold uppercase">{title}</h1>
-            <div>
-                <Button title={showAddTask?"Close":"Add"} onClick={onShowAddTask} 
-                    valClass={showAddTask?"bg-red-600 text-white":"bg-green-600 text-white"}/>
-            </div>
+            {location.pathname==='/' && (
+                <div>
+                    <Button title={showAddTask?"Close":"Add"} onClick={onShowAddTask} 
+                        valClass={showAddTask?"bg-red-600 text-white":"bg-green-600 text-white"}/>
+                </div>
+            )}
         </section>
     );
 }
